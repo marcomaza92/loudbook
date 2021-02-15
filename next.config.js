10a061754c14871285dc4f8dc27b6733eec5a442
Marco Maza <marcoagustinmaza@gmail.com>
2021-02-15 03:40:14 -0300

    chore(app): setup application

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    API_TOKEN: process.env.API_TOKEN,
+    SPACE_ID: process.env.SPACE_ID,
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    CONTENT_TYPE_ID: process.env.CONTENT_TYPE_ID
+  },
+}
